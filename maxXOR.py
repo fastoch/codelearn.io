7211ad249f97d0f9542b2c9d693c722ab50a2661
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-03-31 16:40:18 +0200

    Create maxXOR.py

---
+++ maxXOR.py
@@ -0,0 +1,13 @@
+"""
+https://codelearn.io/training/detail/131003
+
+Given two integers, l and r, find the maximal value of a xor b, where a and b satisfy the following condition:
+
+l <= a <= b <= r 
+
+For l=10 and r=12 the output should be maxXOR(l,r)=7
+
+For l=11 and r=100 the output should be maxXOR(l,r)=127
+
+"""
+
