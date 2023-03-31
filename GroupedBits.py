d587d31eb7b9a93e08105662f65811632c0aaab9
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-03-31 13:49:56 +0200

    Update GroupedBits.py

--- GroupedBits.py
+++ GroupedBits.py
@@ -7,5 +7,10 @@ def groupedBits(n):
             num_groups_1 += 1
     return num_groups_1
     
-# issue: counting 1 bits individually, even grouped ones
-# clue: https://www.w3schools.com/python/trypython.asp?filename=demo_regex_findall
+    
+# possible issue: counting 1 bits individually, even grouped ones
+
+# try this code on https://www.w3schools.com/python/trypython.asp?filename=demo_regex_findall
+    # txt = "10011101011"
+    # x = re.findall("1+", txt)
+    # print(x)
