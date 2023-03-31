695239607fb7fb288f55e36a6479101ad50a868e
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-03-31 19:05:06 +0200

    Update maxXOR.py

--- maxXOR.py
+++ maxXOR.py
@@ -12,7 +12,8 @@ For l=11 and r=100 the output should be maxXOR(l,r)=127
 """
 
 def maxXOR(l,r):
-    list = []                   # list which will contain the results of all XOR operations
-    for i in range(l, r):     
-        list.append(i^r)        # add the result of each xor operation to my list
-    return max(list)   
+    list = []                       # list which will contain the results of all XOR operations
+    for i in range(l, r):  
+        for j in range(l+1, r+1):   
+            list.append(i^j)        # add the result of each xor operation to my list
+    return max(list)     
