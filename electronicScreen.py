7fe7fd32674c1ebd030c09e9fb9f863f86ef418b
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-04-01 22:15:45 +0200

    Update electronicScreen.py

--- electronicScreen.py
+++ electronicScreen.py
@@ -3,5 +3,22 @@ https://codelearn.io/training/detail/326337
 see details in electronicScreen.png
 """
 
+from textwrap import wrap
+
 def electronicScreen(s):
+    s = wrap(s, 8)      # splitting the string into chunks of 8 characters
+
+    switch:
+        case(s == '')   # 0
+                        # 1
+                        # 2
+                        # 3
+                        # 4
+                        # 5
+                        # 6
+                        # 7
+                        # 8
+                        # 9
+        
+    return 
     
