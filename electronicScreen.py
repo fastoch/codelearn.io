c9db5acc5a1a0bb492d63f3f6dda9f9b724c7f54
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-04-02 18:54:11 +0200

    Update electronicScreen.py

--- electronicScreen.py
+++ electronicScreen.py
@@ -7,20 +7,19 @@ from textwrap import wrap
 
 def electronicScreen(s):
     split_str = wrap(s, 8)      # splitting the string into chunks of 8 characters
-    list = []                   # list which will contain different digits
+    digits = ""                 # empty string which will contain the final number
 
-    for i in range(1, len(s)):
+    for i in split_str:
         switch:
-            case(s[i] == '')    # 0
-            case(s[i] == '')                 # 1
-                            # 2
-                            # 3
-                            # 4
-                            # 5
-                            # 6
-                            # 7
-                            # 8
-                            # 9
+            case(split_str[i] == '')    # 0
+            case(split_str[i] == '')    # 1
+            case(split_str[i] == '')    # 2
+            case(split_str[i] == '')    # 3
+            case(split_str[i] == '')    # 4
+            case(split_str[i] == '')    # 5
+            case(split_str[i] == '')    # 6
+            case(split_str[i] == '')    # 7
+            case(split_str[i] == '')    # 8
+            case(split_str[i] == '')    # 9
         
     return 
-    
