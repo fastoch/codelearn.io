63139e8302532240e49390a7d1a615fb089a7b0c
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-04-02 19:18:48 +0200

    Update electronicScreen.py

--- electronicScreen.py
+++ electronicScreen.py
@@ -6,30 +6,30 @@ see details in electronicScreen.png
 from textwrap import wrap
 
 def electronicScreen(s):
-    split_str = wrap(s, 8)      # splitting the string into chunks of 8 characters
+    split_str = wrap(s, 8)      # split the string into chunks of 8 characters and put them into a list
     digits = ""                 # empty string which will contain the final number
 
     for i in split_str:
-        match split_str:
-            case'01011111':
-                digits + 0
+        match i:
+            case '01011111':
+                digits + '0'
             case '00000101':
-                digits + 1
+                digits + '1'
             case '01110110':
-                digits + 2
+                digits + '2'
             case '01110101'):
-                digits + 3
+                digits + '3'
             case '00101101':
-                digits + 4
+                digits + '4'
             case '01111001':
-                digits + 5
+                digits + '5'
             case '01111011':
-                digits + 6 
+                digits + '6' 
             case '01000101':
-                digits + 7
+                digits + '7'
             case '01111111':
-                digits + 8
+                digits + '8'
             case '01111101':
-                digits + 9
+                digits + '9'
         
     return digits
