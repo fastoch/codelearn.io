589bca039402514bedf48dfe055940fc1039e94f
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-04-03 21:10:47 +0200

    Update countBitsPrime.py

--- countBitsPrime.py
+++ countBitsPrime.py
@@ -3,7 +3,7 @@ def count_bits_prime(L,R):
     count_bit_1 = 0     # keeps track of the number of bits set to 1 for each integer between L and R
     is_prime = 0        # keeps track of the number of count_bit_1 that are prime 
 
-    for i in range(L, R+1):
+    for i in range(L,R+1):
         conv = str(bin(i))              # convert numbers into binaries and binaries into strings
         myList = myList.append(conv)    # Then, feed my list with those strings 
                                                 
@@ -11,10 +11,11 @@ def count_bits_prime(L,R):
             for j in range(len(i)):     # check every single character
                 if myList[j] == "1":    # if it's a 1
                     count_bit_1 += 1    # increment count_bit_1
-            # TODO: 
-            # if count_bit_1 is prime:
-            #   is_prime += 1           # increment is_prime
+             
+            for i in range(2,count_bit_1):
+                if (count_bit_1 % i) != 0:      # if count_bit_1 is prime:
+                    is_prime += 1               # increment is_prime
 
-            count_bit_1 = 0             # reset count_bit_1
+            count_bit_1 = 0             # reset count_bit_1 for checking next number 
 
     return is_prime
