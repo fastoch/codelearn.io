c81008ef1daf7c5baca67a1e539fa39f7708fe17
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-04-04 09:07:10 +0200

    Update countBitsPrime.py

--- countBitsPrime.py
+++ countBitsPrime.py
@@ -1,8 +1,8 @@
 def count_bits_prime(L,R):
-    myList = []         
-    count_bit_1 = 0         # keeps track of the number of bits set to 1 for each integer between L and R
-    is_prime = False        # check for each number if its corresponding count_bit_1 is True
-    count_is_prime = 0      # counts how many binary numbers between L and R have a count_bit_1 that is prime
+    myList = []             # will contain strings representing binary numbers between L and R
+    count_bit_1 = 0         # counts the number of bits set to 1 for each integer between L and R
+    remainder = 0           # keeps track of remainders for each modulo operation on count_bit_1 
+    count_is_prime = 0      # counts how many binary numbers between L and R have a count_bit_1 that is prime  
 
     for number in range(L, R+1):
         conv = str(bin(number))              # convert numbers into binaries and binaries into strings
@@ -15,10 +15,11 @@ def count_bits_prime(L,R):
 
             # checking if current count_bit_1 is prime 
             for n in range(2, count_bit_1):
-                if (count_bit_1 % n) == 0:      
-                    is_prime = False
-                else 
-                    is_prime = True             
+                remainder += (count_bit_1 % n)
+            # incrementing counter if it's prime
+            if remainder == 0:
+                count_is_prime += 1
+                             
 
             count_bit_1 = 0             # reset count_bit_1 for checking next item in my list 
 
