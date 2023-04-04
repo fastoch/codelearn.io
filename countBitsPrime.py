1f92f3c367320951987388aa5b7a710fc6f3371a
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-04-04 08:43:55 +0200

    Update countBitsPrime.py

--- countBitsPrime.py
+++ countBitsPrime.py
@@ -1,21 +1,23 @@
 def count_bits_prime(L,R):
     myList = []         
-    count_bit_1 = 0     # keeps track of the number of bits set to 1 for each integer between L and R
-    is_prime = 0        # keeps track of the number of count_bit_1 that are prime 
+    count_bit_1 = 0         # keeps track of the number of bits set to 1 for each integer between L and R
+    is_prime = False        
+    count_is_prime = 0
 
-    for i in range(L,R+1):
-        conv = str(bin(i))              # convert numbers into binaries and binaries into strings
-        myList = myList.append(conv)    # Then, feed my list with those strings 
+    for number in range(L, R+1):
+        conv = str(bin(number))              # convert numbers into binaries and binaries into strings
+        myList = myList.append(conv)         # Then, feed my list with those strings 
                                                 
-        for i in myList:                # for each string/binary in myList
-            for j in range(len(i)):     # check every single character
-                if myList[j] == "1":    # if it's a 1
-                    count_bit_1 += 1    # increment count_bit_1
-             
-            for i in range(2,count_bit_1):
-                if (count_bit_1 % i) != 0:      # if count_bit_1 is prime:
-                    is_prime += 1               # increment is_prime
+        for item in myList:                 # for each string/binary in myList
+            for i in range(0, len(i)):      # check every single character
+                if myList[i] == "1":        # if it's a 1
+                    count_bit_1 += 1        # increment count_bit_1
 
-            count_bit_1 = 0             # reset count_bit_1 for checking next number 
+            # checking if count_bit_1 is prime 
+            for n in range(2, count_bit_1):
+                if (count_bit_1 % n) == 0:      
+                    is_prime += 1              
 
-    return is_prime
+            count_bit_1 = 0             # reset count_bit_1 for checking next item in my list 
+
+    return count_is_prime
