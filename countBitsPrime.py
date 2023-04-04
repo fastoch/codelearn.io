39dd3255ae9cc6c212c446d6d26764fa28b3c6c2
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-04-04 10:46:30 +0200

    Update countBitsPrime.py

--- countBitsPrime.py
+++ countBitsPrime.py
@@ -1,7 +1,7 @@
 def count_bits_prime(L,R):
     myList = []             # will contain strings representing binary numbers between L and R
     count_bit_1 = 0         # counts the number of bits set to 1 for each integer between L and R
-    remainders = 0          # keeps track of remainders for each modulo operation on count_bit_1 
+    is_prime = False        # flag variable for checking if each count_bit_1 is prime
     count_is_prime = 0      # counts how many binary numbers between L and R have a count_bit_1 that is prime  
 
     for number in range(L, R+1):
@@ -17,18 +17,17 @@ def count_bits_prime(L,R):
         print(count_bit_1)
 
         # checking if current count_bit_1 is prime 
-        if count_bit_1 == 0 or count_bit_1 == 1:    # getting rid of special cases 0 and 1
-            count_is_prime = 0                      # 0 and 1 have no dividers but are not considered prime numbers
-        else:
+        if count_bit_1 > 1:                         # 0 and 1 have no dividers but are not considered prime numbers
             for n in range(2, count_bit_1 + 1):     # first arg is inclusive, but second arg of range is exclusive, hence the +1 
-                remainders += (count_bit_1 % n)
-            # incrementing counter if it's prime (if all remainders are equal to 0)
-            print("Remainders:", remainders)
-            if remainders == 0:
-                count_is_prime += 1
-                                
+                if (count_bit_1 % n) == 0:
+                    count_is_prime += 1
+        else: 
+            count_is_prime = 0
+
         count_bit_1 = 0        # reset count_bit_1 for checking next item in my list 
 
     return count_is_prime
 
 print("Result: ", count_bits_prime(6,10))
+
+# remove print statements when my program works
